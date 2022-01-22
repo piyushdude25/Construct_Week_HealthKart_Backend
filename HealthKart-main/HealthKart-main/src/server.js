0ccb12d63211b141b1af56f95517a02136981eed
Premkumar-hulikoppe <premuhulikoppe@gmail.com>
2022-01-22 20:11:05 +0530

    changes added
    changes added

--- HealthKart-main/HealthKart-main/src/server.js
+++ HealthKart-main/HealthKart-main/src/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require('dotenv').config();
-const Razorpay = require("razorpay");
+const port = process.env.PORT || 2500;
 
 
 
@@ -28,78 +28,7 @@ app.use("/carts/", cartController);
 app.use("/products/", productController);
 
 
-// ------Google Authenticaton Feature---//
-
-// const passport = require("passport");
-// const {Strategy} = require("passport-google-oauth20");
-// const session = require('express-session')
-// app.set('trust proxy', 1) // trust first proxy
-// app.use(session({
-//   secret: 'keyboard cat',
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: { secure: true }
-// }))
-
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// passport.serializeUser(function (user, cb) {
-//     cb(null, user);
-// });
-
-// passport.deserializeUser(function (obj, cb){
-//     cb(null, obj);
-// });
-
-// passport.use(new Strategy({
-//     clientID : "344648579890-8qum84baej1958nll78vl54lhs0j6ctu.apps.googleusercontent.com",
-//     clientSecret: "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",
-//     callbackURL : "http://localhost:2500/auth/google/callback"
-// },
-// function (accessToken, refreshToken, profile, done){
-//     // if user is already exist in your database login otherwise // save data and signup
-//     done(null, {});
-// }
-// ));
-
-// app.get("/auth/google/", passport.authenticate("google", {scope: ["profile"]}));
-
-// app.get("/auth/google/callback/", passport.authenticate("google", {failureRedirect: "/auth/fail/"}), (req, res , next) => {
-//     console.log(req.user, req.isAuthenticated());
-//     res.send("user log in successful");
-// })
-
-// app.get("/auth/fail", (req, res, next) => {
-// res.send("user logged in failed");
-// });
-
-// app.get("/logout/", (req, res, next) => {
-//     req.logout();
-//     console.log(req.isAuthenticated());
-//     res.send("user is logged out");
-// });
-
-// const razorpay = new Razorpay({
-//     key_id: "rzp_test_3r9tWvFFZv3Lix",
-//     key_secret: "XyujMP3HyDer7YtX9wVJCtbq"
-// })
-
-// app.post("/order/", (req, res) =>{
-//     let  options = {
-//   amount: 1 * 100,
-//   currency: "INR",
-// };
-// razorpay.orders.create(options, function(err, order){
-//     console.log(order);
-// })
-// )
-
-
-
-
-
-app.listen(2500, async() => {
+app.listen(port, async() => {
     try{
         await connect();
         console.log("Port 2500 is Listening");
